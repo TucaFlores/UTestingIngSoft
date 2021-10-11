e3956b80020cecece81f1b6608a3fd6d5f765080
Sebastian <fsebastian0002@gmail.com>
2021-10-11 12:58:14 -0300

    Actualizaciones

--- src/test/java/com/ingsoft/Carrito.java
+++ src/test/java/com/ingsoft/Carrito.java
@@ -2,16 +2,30 @@ package com.ingsoft;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Carrito {
 
-    private float montoTotal;
+    private double montoTotal;
 
-    private List<DetalleCarrito> detalles;
+    private List<DetalleCarrito> detalles = new ArrayList<>();
+
+    public double montoCarrito(List<DetalleCarrito> detalles){
+        for (DetalleCarrito detalleCarrito : detalles) {
+            this.montoTotal = this.montoTotal + (detalleCarrito.getArticulo().getPrecio())*(detalleCarrito.getCantArticulo());
+        }
+        return montoTotal;
+    }
+
+    public void addDetalles(DetalleCarrito detalleCarrito) {
+        this.detalles.add(detalleCarrito);
+    }
 }
