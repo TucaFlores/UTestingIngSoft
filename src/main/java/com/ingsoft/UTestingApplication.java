42ea57aa620f040e0631dcc7f496fdf0efdad2df
Sebastian <fsebastian0002@gmail.com>
2021-10-11 10:54:19 -0300

    Commit Incial

---
+++ src/main/java/com/ingsoft/UTestingApplication.java
@@ -0,0 +1,13 @@
+package com.ingsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsoft/UTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
